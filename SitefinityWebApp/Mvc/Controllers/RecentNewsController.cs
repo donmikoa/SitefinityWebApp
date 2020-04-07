5a8b819e167f0ec8925d07c568f7ad4b8e359820
Michael Iyaomolere <michael.iyaomolere@snhu.edu>
2020-04-07 11:29:20 -0400

    Creating recent news widget.

---
+++ SitefinityWebApp/Mvc/Controllers/RecentNewsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SitefinityWebApp.Mvc.Controllers
+{
+    public class RecentNewsController : Controller
+    {
+        // GET: RecentNews
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
