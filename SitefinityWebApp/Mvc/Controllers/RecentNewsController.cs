c1cfe26de4bb74abc9277aeb63d9c9cd4f6eccc2
Michael Iyaomolere <michael.iyaomolere@snhu.edu>
2020-04-07 11:43:03 -0400

    Creating the Recent News Controller.

--- SitefinityWebApp/Mvc/Controllers/RecentNewsController.cs
+++ SitefinityWebApp/Mvc/Controllers/RecentNewsController.cs
@@ -3,14 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Telerik.Sitefinity.Mvc;
 
 namespace SitefinityWebApp.Mvc.Controllers
 {
+    [ControllerToolboxItem(Name = "Recent News", Title = "Recent News", SectionName = "MvcWidgets")]
     public class RecentNewsController : Controller
     {
         // GET: RecentNews
         public ActionResult Index()
         {
+
             return View();
         }
     }
---
+++ SitefinityWebApp/Mvc/Models/RecentNewsModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Telerik.Sitefinity.News.Model;
+
+namespace SitefinityWebApp.Mvc.Models
+{
+    public class RecentNewsModel
+    {
+        public RecentNewsModel(IQueryable<NewsItem> items)
+        {
+            this.Items = items;
+        }
+        public IQueryable<NewsItem> Items
+        {
+            get;
+            private set;
+        }
+    }
+}
